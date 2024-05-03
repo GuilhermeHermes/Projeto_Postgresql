ba3a4f0b49715a9a83c6d34ad3990effc5bc1f06
Guilherme Hermes <guilherme.hermes.053@ufrn.edu.br>
2024-05-03 00:01:37 -0300

    atualizando DER

--- ProjetoDB/Scripts/view_salarios_funcionarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW view_salarios_funcionarios AS
-SELECT
-    F.idFuncionario,
-    P.nomePessoa,
-    F.salarioFuncionario
-FROM
-    Funcionario F
-    JOIN Pessoa P ON F.Pessoa_idPessoa = P.idPessoa;
--- ProjetoDB/Scripts/view_transacoes_agrupadas_tipo.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW view_transacoes_agrupadas_tipo AS
-SELECT
-    tipoTransacao,
-    SUM(valorTransacao) AS total_valor
-FROM
-    Transacoes
-GROUP BY
-    tipoTransacao;
